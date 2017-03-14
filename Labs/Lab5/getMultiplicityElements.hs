05be89f27052a916f36e968dd43f50c164e3bdcd
JakimychevNikolaj <jakimychev.nikolaj@gmail.com>
2017-03-14 11:59:06 +0300

    Update getMultiplicityElements.hs

--- Labs/Lab5/getMultiplicityElements.hs
+++ Labs/Lab5/getMultiplicityElements.hs
@@ -1,5 +1,9 @@
 getMultiplicityElements :: Int -> Int -> Int -> [Int]
 getMultiplicityElements startWith amount multiplicity = [ x | x <- [startWith..amount*multiplicity], ((x `mod` multiplicity) == 0) ]
 
--- main = do
---     print (getMultiplicityElements 4 7 241)
+main = do
+    putStrLn "Hello input a starting point amount and multiplicity: "
+    startWith <- getLine
+    amount <- getLine
+    multiplicity <- getLine
+    print (getMultiplicityElements (read startWith) (read amount) (read multiplicity))
